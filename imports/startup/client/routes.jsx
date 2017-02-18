9b5d2ea54e21d8bbdab1b393ed07963fec69d386
ClaaziX <georgecwnn@hotmail.com>
2017-02-18 18:37:48 +0000

    yoyo

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 
 import AppHeader from '../../ui/components/AppHeader';
 import GridListTab from '../../ui/components/GridListTab';
+import ItemCreation from '../../ui/components/ItemCreation';
 import AccountsUIWrapper from '../../ui/AccountsUIWrapper';
 import MapViewContainer from '../../ui/containers/MapViewContainer';
 import YourItemsContainer from '../../ui/containers/YourItemsContainer';
@@ -36,6 +37,10 @@ const renderRoutes = () => (
       <Route
         path='/FoodComments/:foodID'
         component={FoodItemCommentsContainer}
+      />
+      <Route
+        path='/ItemCreation'
+        component={ItemCreation}
       />
     </Route>
   </Router>
