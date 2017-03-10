9a5961748efa845d9fbaf7397d0ae6ad9cf11e01
ClaaziX <georgecwnn@hotmail.com>
2017-03-10 14:05:58 +0000

    Logging in! :)

--- imports/ui/components/Images/PhotoUpload.jsx
+++ imports/ui/components/Images/PhotoUpload.jsx
@@ -10,6 +10,7 @@ export default class PhotoUpload extends React.Component{
 
 	constructor(props) {
 		super(props);
+		this.onDrop = this.onDrop.bind(this);
 		this.state = {
 				imageUpload:'',
 				open: false,
@@ -19,46 +20,49 @@ export default class PhotoUpload extends React.Component{
 	onDrop(file){
 		console.log("onDrop called")
 	//A call back to handle the error with a toast
-		var callBackError = (function(err){
-			console.log("callback error called")
-			console.log(err)
-			this.setState({open:true})
-		}).bind(this);
+		// var callBackError = (function(err){
+		// 	console.log("callback error called")
+		// 	console.log(err)
+		// 	this.setState({open:true})
+		// }).bind(this);
 
-		//This function is here just so we can actually push to scope
-		var callBackState = (function(imageUrl){
-			console.log("callBackState called")
-			console.log(imageUrl);
-			this.setState({imageUpload:imageUrl});
-			this.props.onUpload(imageUrl);
-		}).bind(this);
+		// //This function is here just so we can actually push to scope
+		// var callBackState = (function(imageUrl){
+		// 	console.log("callBackState called")
+		// 	console.log(imageUrl);
+		// 	this.setState({imageUpload:imageUrl});
+		// 	console.log(this.state.imageUpload)
+		// }).bind(this);
 
-		console.log("images...")
-		console.log(file)
-		console.log(file[0])		
-		var images = [];
-		//Initiate insert and get fileObj handle
-		images.push(file[0], function(err,fileObj){
-			if(err){
-				//do something to handle errors that get thrown
-				callBackError(err)
-				console.log(err)
-				console.log("didnt like it")
-			} else {
-				//Check the database and find out when all data is uploaded then call the call back
-				console.log("attempting...")
-				var liveQuery = images.find(fileObj._id).observe({
-					changed : function(newImage, OldImage){
-						if (newImage.url() != null){
-							liveQuery.stop();
-							console.log(newImage.url())
-							callBackState(newImage.url());
-						}
-					}
-				});
-			}
-		});
+		this.setState({imageUpload:file[0].preview});
+		this.props.onUpload(file[0].preview)
+		console.log(this.state.imageUpload)
+		// //Initiate insert and get fileObj handle
+		// images.push(file, function(err,fileObj){
+		// 	console.log("images.push called..")
+		// 	if(err){
+		// 		//do something to handle errors that get thrown
+		// 		callBackError(err)
+		// 		console.log(err)
+		// 		console.log("didnt like it")
+		// 	} else {
+		// 		//Check the database and find out when all data is uploaded then call the call back
+		// 		console.log("attempting...")
+		// 		var liveQuery = images.find(fileObj._id).observe({
+		// 			changed : function(newImage, OldImage){
+		// 				if (newImage.url() != null){
+		// 					liveQuery.stop();
+		// 					console.log(newImage.url())
+		// 					callBackState(newImage.url());
+		// 				}
+		// 			}
+		// 		});
+		// 	}
+		// });
 	}
+
+
+
 
 	render() {
 		return (
@@ -67,7 +71,7 @@ export default class PhotoUpload extends React.Component{
 					{this.state.imageUpload == '' ?
 						<div>Drop files here for upload</div>
 					:
-						<img height="100%" src={this.state.imageUpload} />}
+						<img style={{display: "block", margin: "0 auto"}} height="100%" src={this.state.imageUpload} />}
 				</Dropzone>
 				<Snackbar
 					open={this.state.open}
