3b752ab7fdbddfa81db10b3f93ae03867189ca7b
thelxinoe <thomas.nickson@gmail.com>
2017-03-12 15:17:05 +0000

    completes the private mesages opening

--- imports/ui/components/Comment.jsx
+++ imports/ui/components/Comment.jsx
@@ -2,40 +2,40 @@ import React from 'react';
 
 Comment = React.createClass({
 
-render(){
-	var container = "commentFlexOther-container";
-	var item = "commentFlex-item";
-	if (this.props.username == this.props.user){
-		container = "commentFlexUser-container";
-		item = "commentFlexUser-item";
-	}
-	return(
-		<div>
-			{ this.props.same ?
-				""
-			:
-				<div style={{height: "10px"}}></div>
-			}
-			<div className={container}>
-				<div className={item}>
-					{ this.props.same ?
-						<div style={{height: "4px", width: "40px"}}>
-						</div>
-					:
-						<div>
-							<img className="avatar" src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png" />
-							<br/>
-							{this.props.date}
-						</div>
-					}
-				</div>
-				<div className={item}>
-					{this.props.comment}
-				</div>
-			</div>
-		</div>
-	);
-}
+  render(){
+    var container = "commentFlexOther-container";
+    var item = "commentFlex-item";
+    if (this.props.username == this.props.user){
+      container = "commentFlexUser-container";
+      item = "commentFlexUser-item";
+    }
+    return(
+      <div>
+        { this.props.same ?
+          ""
+          :
+          <div style={{height: "10px"}}></div>
+        }
+        <div className={container}>
+          <div className={item}>
+            { this.props.same ?
+              <div style={{height: "4px", width: "40px"}}>
+              </div>
+              :
+              <div>
+                <img className="avatar" src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png" />
+                <br/>
+                {this.props.date}
+              </div>
+            }
+          </div>
+          <div className={item}>
+            {this.props.comment}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 });
 
--- imports/ui/components/Comments.jsx
+++ imports/ui/components/Comments.jsx
@@ -1,30 +1,35 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 
 import Comment from './Comment';
 import TimeSince from './TimeSince.jsx';
 
 class Comments extends React.Component {
 
-  render () {
-    console.log(this.props.comments)
-    return this.props.comments.map((comment) => {
-      var prvUsr;
-      var currUsr = comment.username;
-      var same = false;
-      if(currUsr === prvUsr){
-        same = true;
-      }else{prvUsr = currUsr;}
-      prvUsr = currUsr;
-      return(
-        <Comment
-          comment={comment.comment}
-          date={<TimeSince time={comment.createdAt} />}
-          username={currUsr}
-          same={same}
-        />
-      )
-    });
-  }
+    render() {
+        console.log(this.props.comments)
+
+        return (
+            <div>
+                {this.props.comments.map((comment) => {
+                    var prvUsr;
+                    var currUsr = comment.username;
+                    var same = false;
+                    if (currUsr === prvUsr) {
+                        same = true;
+                    } else {
+                        prvUsr = currUsr;
+                    }
+                    prvUsr = currUsr;
+                    return (
+                        <Comment comment={comment.comment} date={< TimeSince time = {
+                            comment.createdAt
+                        } />} username={currUsr} same={same}/>
+                    )
+                })}
+            </div>
+        )
+
+    }
 }
 
 export default Comments;
